6ea157b71650a32883f7e48b418f49417ccf6686
Darsh177 <165510332+Darsh177@users.noreply.github.com>
2026-08-18 11:26:46 +0300

    Delete task7 directory

--- task7/src/main/java/com/example/demo/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Employee;
-import org.springframework.data.repository.ListCrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends ListCrudRepository<Employee , Long> {
-}
